1ea1a98fc718e3d174f331e4df0eb85e197786f3
Joyel99Dev <164185653+McpeDX@users.noreply.github.com>
2025-09-14 16:18:18 +0530

    Update DeadCodeInjector.java

--- src/main/java/com/myobf/transformer/DeadCodeInjector.java
+++ src/main/java/com/myobf/transformer/DeadCodeInjector.java
@@ -3,20 +3,30 @@ package com.myobf.transformer;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.*;
 
+import java.util.Random;
+
 public class DeadCodeInjector implements Transformer, Opcodes {
+
+    private final Random random = new Random();
 
     @Override
     public void transform(ClassNode cn) {
         for (MethodNode mn : cn.methods) {
-            // Skip abstract methods and constructors
-            if ((mn.access & ACC_ABSTRACT) != 0 || mn.name.equals("<init>") || mn.name.equals("<clinit>")) {
+            // Skip abstract methods, constructors, or native methods
+            if ((mn.access & ACC_ABSTRACT) != 0 ||
+                (mn.access & ACC_NATIVE) != 0 ||
+                mn.name.equals("<init>") ||
+                mn.name.equals("<clinit>")) {
                 continue;
             }
 
-            // Find all return statements
-            for (AbstractInsnNode insn : mn.instructions) {
+            // Inject at method entry
+            mn.instructions.insertBefore(mn.instructions.getFirst(), createRandomDeadCodeBlock());
+
+            // Inject before each return statement
+            for (AbstractInsnNode insn : mn.instructions.toArray()) {
                 if (isReturn(insn.getOpcode())) {
-                    mn.instructions.insertBefore(insn, createDeadCodeBlock());
+                    mn.instructions.insertBefore(insn, createRandomDeadCodeBlock());
                 }
             }
         }
@@ -26,26 +36,78 @@ public class DeadCodeInjector implements Transformer, Opcodes {
         return opcode >= IRETURN && opcode <= RETURN;
     }
 
-    private InsnList createDeadCodeBlock() {
+    private InsnList createRandomDeadCodeBlock() {
+        switch (random.nextInt(3)) {
+            case 0:
+                return createDeadCodeBlockAlwaysFalse();
+            case 1:
+                return createDeadCodeBlockLoop();
+            default:
+                return createDeadCodeBlockException();
+        }
+    }
+
+    // Dead code with an always-false condition
+    private InsnList createDeadCodeBlockAlwaysFalse() {
         InsnList list = new InsnList();
         LabelNode label = new LabelNode();
 
-        // An if statement that is always false
-        list.add(new InsnNode(ICONST_0)); // push false
+        list.add(new InsnNode(ICONST_0)); // false
         list.add(new JumpInsnNode(IFEQ, label)); // jump if false
 
-        // A block of junk code that will never be executed
-        list.add(new LdcInsnNode("This is dead code"));
+        list.add(new LdcInsnNode("Dead code block"));
         list.add(new InsnNode(POP));
-        list.add(new TypeInsnNode(NEW, "java/lang/RuntimeException"));
+
+        list.add(label);
+        return list;
+    }
+
+    // Dead code with an infinite-looking but harmless loop
+    private InsnList createDeadCodeBlockLoop() {
+        InsnList list = new InsnList();
+        LabelNode start = new LabelNode();
+        LabelNode end = new LabelNode();
+
+        list.add(start);
+        list.add(new InsnNode(ICONST_0));
+        list.add(new InsnNode(POP)); // dummy operation
+        list.add(new JumpInsnNode(GOTO, end)); // jump to end
+
+        list.add(new LdcInsnNode("This loop does nothing"));
+        list.add(new InsnNode(POP));
+
+        list.add(end);
+        return list;
+    }
+
+    // Dead code that throws an exception but never gets executed
+    private InsnList createDeadCodeBlockException() {
+        InsnList list = new InsnList();
+        LabelNode label = new LabelNode();
+
+        list.add(new InsnNode(ICONST_0)); // false
+        list.add(new JumpInsnNode(IFNE, label)); // jump if true
+
+        // Throw an exception
+        list.add(new TypeInsnNode(NEW, "java/lang/IllegalStateException"));
         list.add(new InsnNode(DUP));
-        list.add(new LdcInsnNode("You should not see this"));
-        list.add(new MethodInsnNode(INVOKESPECIAL, "java/lang/RuntimeException", "<init>", "(Ljava/lang/String;)V"));
+        list.add(new LdcInsnNode(encryptString("This should never happen")));
+        list.add(new MethodInsnNode(INVOKESPECIAL, "java/lang/IllegalStateException", "<init>", "(Ljava/lang/String;)V", false));
         list.add(new InsnNode(ATHROW));
 
-        // The label that is the target of the jump
         list.add(label);
-
         return list;
+    }
+
+    // Simple XOR encryption for demonstration
+    private String encryptString(String input) {
+        char key = (char) (random.nextInt(26) + 'A');
+        StringBuilder sb = new StringBuilder();
+        for (char c : input.toCharArray()) {
+            sb.append((char) (c ^ key));
+        }
+        // For simplicity, just append the key as the last character
+        sb.append(key);
+        return sb.toString();
     }
 }
